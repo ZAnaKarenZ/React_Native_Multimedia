9d813443507bc336983617baf46f38a097831dc9
ZAnaKarenZ <117191903+ZAnaKarenZ@users.noreply.github.com>
2024-11-19 20:42:06 -0600

    first commit

--- MusicApp/app/(tabs)/index.tsx
+++ MusicApp/app/(tabs)/index.tsx
@@ -1,52 +1,37 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
+import { Image, StyleSheet, Platform, Text, View } from 'react-native';
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import React, { useEffect } from 'react';
+import * as SQLite from 'expo-sqlite';
 
 export default function HomeScreen() {
+  const db = SQLite.openDatabaseSync('multimedia.db');
+  useEffect(() => {
+    const createTable = async() =>{
+      try {
+        await db.execAsync(`
+          PRAGMA journal_mode = WAL;
+          CREATE TABLE IF NOT EXISTS omatopopih
+          (id INTEGER PRIMARY KEY NOT NULL,
+          value TEXT NOT NULL, 
+          intValue INTEGER);
+          INSERT INTO omatopopih (value, intValue) VALUES ('test1', 123);
+          INSERT INTO omatopopih (value, intValue) VALUES ('test2', 456);
+          INSERT INTO omatopopih (value, intValue) VALUES ('test3', 789);
+          `);
+
+      } catch (error) {
+        console.error(error);
+      }
+    };
+  }, []);
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View>
+      <Text>Hello from DatosSqlite!</Text>
+    </View>
   );
 }
 
---
+++ MusicApp/database/DatosSqlite.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, ScrollView, Button, TouchableHighlight } from 'react-native';
+//import * as SQLite from 'expo-sqlite';
+import SQLite from 'react-native-sqlite-storage';
+
+export default function DatosSqlite() {
+  return (
+    <View>
+      <Text>Hello from DatosSqlite!</Text>
+    </View>
+  );
+}
+
